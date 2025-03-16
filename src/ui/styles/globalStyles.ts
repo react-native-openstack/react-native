84dd0aba995fd404cf7ac3cd5bbbc483cb89b865
홍지성 <popomonclub@gmail.com>
2025-03-16 20:35:58 +0900

    refactor: 기준 해상도 변경 및 폰트 객체 샘플 추가

--- src/ui/styles/globalStyles.ts
+++ src/ui/styles/globalStyles.ts
@@ -1,9 +1,9 @@
 import {Dimensions} from 'react-native';
 
-// 디자인 화면 크기 (Figma)
+// 디자인 화면 크기 (6.1 인치)
 const basicDimensions = {
-  width: 360,
-  height: 640,
+  width: 393,
+  height: 852,
 };
 
 const {width: initialWidth, height: initialHeight} = Dimensions.get('window');
@@ -21,3 +21,15 @@ export const hp = (value: number) =>
 
 export const wp = (value: number) =>
   Math.floor((screenWidth / basicDimensions.width) * value);
+
+export enum Font {
+  Thin = '100_Inter_Thin',
+  ExtraLight = '200_Inter_ExtraLight',
+  Light = '300_Inter_Light',
+  Regular = '400_Inter_Regular',
+  Medium = '500_Inter_Medium',
+  SemiBold = '600_Inter_SemiBold',
+  Bold = '700_Inter_Bold',
+  ExtraBold = '800_Inter_ExtraBold',
+  Black = '900_Inter_Black',
+}
