ad6e9a5cad9d8cfc10975a11349064e630835adb
홍지성 <popomonclub@gmail.com>
2025-03-16 20:30:57 +0900

    refactor: 필요한 번역문구 및 리소스 추가

---
+++ src/ui/assets/icon/index.ts
@@ -0,0 +1,17 @@
+export {default as AnalysisActiveIcon} from './files/analysis_active.svg';
+export {default as AnalysisIcon} from './files/analysis.svg';
+export {default as ArrowLeftIcon} from './files/arrow_left.svg';
+export {default as ArrowRightIcon} from './files/arrow_right.svg';
+export {default as BucketActiveIcon} from './files/bucket_active.svg';
+export {default as BucketIcon} from './files/bucket.svg';
+export {default as DeckThumbnailIcon} from './files/deck_thumbnail.svg';
+export {default as FilterFillIcon} from './files/filter_fill.svg';
+export {default as FilterIcon} from './files/filter.svg';
+export {default as FloatingAddIcon} from './files/floating_add.svg';
+export {default as GuestIcon} from './files/guest.svg';
+export {default as MenuIcon} from './files/menu.svg';
+export {default as MinusIcon} from './files/minus.svg';
+export {default as OptionIcon} from './files/option.svg';
+export {default as PlusIcon} from './files/plus.svg';
+export {default as ProfileActiveIcon} from './files/profile_active.svg';
+export {default as ProfileIcon} from './files/profile.svg';
---
+++ src/ui/assets/images/index.ts
@@ -0,0 +1,2 @@
+export const LogoAppleImage = require('./files/logo_apple.png');
+export const LogoGoogleImage = require('./files/logo_google.png');
